443fb80fc58c0728f820b1ede71a9abb442c78e6
Julia Dutra <juliadutragoncalves@gmail.com>
2025-09-07 17:00:55 -0300

    chore: Configura o Alembic para gestão de migrations da base de dados
    Inicializa o repositório do Alembic para
    migrations de banco de dados e configura o
    ambiente para se conectar ao banco de dados da
    aplicação.

--- app/core/config.py
+++ app/core/config.py
@@ -2,7 +2,16 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    USUARIO_BD: str
+    SENHA_BD: str
+    NOME_BD: str
+    HOST_BD: str
+    PORTA_BD: str
 
     TITULO_API: str
+
+    @property
+    def url_bd(self) -> str:
+        return f"postgresql+asyncpg://{self.USUARIO_BD}:{self.SENHA_BD}@{self.HOST_BD}:{self.PORTA_BD}/{self.NOME_BD}"
 
 settings = Settings()
